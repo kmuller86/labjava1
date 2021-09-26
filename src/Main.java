0d4279c2fec7ef1debec504a9c756ac64ff041d4
Rolleksy <aleksanderpurcha@gmail.com>
2021-09-26 13:37:49 +0200

    start

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,14 @@ public class Main {
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
-
+            int liczba = 6;
+            float floa1 = 5.67f;
+            char litera = 'D';
+            boolean mojBool = true;
+            long mojLong = 1908354123L;
+            String tekst = "Dzień dobry, moje dane to: ";
+            String przeci = ", ";
+        System.out.println(tekst + liczba +przeci+ floa1 + przeci+ litera +przeci+  mojBool +przeci+ mojLong);
         /*
         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
         *    mnożenia, dziealenia i modulo na zmiennych:
