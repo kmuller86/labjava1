1e90f4b5068644b79bc0b96e7ab0f0aaf18d1a69
Rolleksy <aleksanderpurcha@gmail.com>
2021-09-26 13:19:33 +0200

    start

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Random rnd = new Random();
         /* Tablice - cw_6 */
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        double[] tablica= new double[5];
+        for (int i = 0; i<=4; i++){
+            tablica[i] = rnd.nextDouble();
+
+        }
+        for (int i = 0; i<=4; i++){
+            System.out.println(tablica[i]);
+        }
 
     }
 }
