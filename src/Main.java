b1b702044469805d52c0c25e9686d21884f7c838
Rolleksy <aleksanderpurcha@gmail.com>
2021-09-26 13:49:42 +0200

    start

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wpisz jakikolwiek wyraz");
+        String wyraz = scan.next();
+        System.out.println(wyraz);
     }
 }
