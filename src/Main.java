9bccdfd13de7d1383bb2ec80d27abac7ce1a2fc5
Rolleksy <aleksanderpurcha@gmail.com>
2021-09-26 13:11:22 +0200

    start

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Scanner scan = new Scanner(System.in);
         /* ćw.5 */
 
         /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
@@ -25,5 +25,15 @@ public class Main {
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
 
+        System.out.println("Podaj liczbe zmiennoprzecinkową");
+        double liczba = scan.nextDouble();
+        if (liczba > 5){
+            int wynik1 = (int)(liczba/liczba);
+            double wynik2 = liczba - (double)wynik1;
+            System.out.println("Liczba jest wieksza od 5 i po przecinku jest: " +wynik2);
+        }
+        else{
+            System.out.println("Liczba jest mniejsza od 5");
+        }
     }
 }
