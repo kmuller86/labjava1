06c9caad22f124022948b62f6460f650ddd97afd
Rolleksy <aleksanderpurcha@gmail.com>
2021-09-28 13:19:47 +0200

    hello

---
+++ src/Main1.java
@@ -0,0 +1,7 @@
+//import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class Main1 {
+    public static void main(String[] args) {
+    System.out.println("Hello World");
+    }
+}
